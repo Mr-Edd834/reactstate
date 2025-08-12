01080a719ab7418e9d8adbb8d1c80098871fd7b9
Mr-Edd834 <kagorieddy76@gmail.com>
2025-08-12 09:36:05 +0300

    donee

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: "Bruce Wayne",
+        bio: "What's my superpower you ask? I'm rich.",
+        imgSrc: "https://queenstudios.shop/cdn/shop/files/1.BruceWayneStatue.png?v=1727835010",
+        profession: "Genius Billionaire Playboy Philanthropist"
+      },
+      shows: false,
+      mountTime: new Date(),
+      timeSinceMount: 0
+    };
+  }
+
+  toggleShow = () => {
+    this.setState(prevState => ({
+      shows: !prevState.shows
+    }));
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const seconds = Math.floor((new Date() - this.state.mountTime) / 1000);
+      this.setState({ timeSinceMount: seconds });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { person, shows, timeSinceMount } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Profile Toggle App</h1>
+        <button onClick={this.toggleShow}>
+          {shows ? 'Hide Profile' : 'Show Profile'}
+        </button>
+
+        {shows && (
+          <div className="profile">
+            <img src={person.imgSrc} alt={person.fullName} width="150" />
+            <h2>{person.fullName}</h2>
+            <p><strong>Profession:</strong> {person.profession}</p>
+            <p><strong>Bio:</strong> {person.bio}</p>
+          </div>
+        )}
+
+        <p className="timer">
+          Component mounted {timeSinceMount} seconds ago
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
